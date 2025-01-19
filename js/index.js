229ca903a98e6df73f7d82adb0e754ed9ad4b652
David Joslin <davidjoslin1990@gmail.com>
2025-01-19 02:41:46 -0500

    edited index and assignment js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+//for loops break
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        break;
+    }
+    console.log(i)
+}
+
+//continue
+for (let = 0; i < 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    console.log(i);
+}
